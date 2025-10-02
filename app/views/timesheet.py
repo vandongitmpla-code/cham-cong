d280e9362b4d4f25a7003a0efd312ab8376b711c
vandong <vandong.itmpla@gmail.com>
2025-10-02 14:53:47 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/views/timesheet.py
+++ app/views/timesheet.py
@@ -10,7 +10,7 @@ from werkzeug.utils import secure_filename
 from app.utils.cleaning import clean_attendance_data
 import datetime
 import pandas as pd
-from flask import send_file
+
 from app.models import Employee, AttendanceLog, Payroll, db
 import re
 @bp.route("/timesheet/<filename>")
