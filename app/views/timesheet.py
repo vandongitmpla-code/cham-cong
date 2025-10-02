4d4067551324ccb069d74a55a829549433b14628
vandong <vandong.itmpla@gmail.com>
2025-10-02 14:53:16 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/views/timesheet.py
+++ app/views/timesheet.py
@@ -4,14 +4,14 @@ import pandas as pd
 from app.utils.cleaning import clean_attendance_data
 from app.models import AttendanceLog, Employee, db
 from . import bp
-from flask import render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 import os
 from werkzeug.utils import secure_filename
 from app.utils.cleaning import clean_attendance_data
 import datetime
 import pandas as pd
 from flask import send_file
-
+import io
 from app.models import Employee, AttendanceLog, Payroll, db
 import re
 @bp.route("/timesheet/<filename>")
