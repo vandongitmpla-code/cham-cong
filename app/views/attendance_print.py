05c79e5763c3b1b0ec966ad43aabe6e12fb65484
vandong <vandong.itmpla@gmail.com>
2025-10-02 15:06:47 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/views/attendance_print.py
+++ app/views/attendance_print.py
@@ -2,8 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 import os
 from werkzeug.utils import secure_filename
 from app.utils.cleaning import clean_attendance_data
-import datetime
-import pandas as pd
+
 from flask import send_file
 from . import bp
 from app.models import db
