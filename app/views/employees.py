a24ff920d1031f6e37b00a308d5af7ac6a8f9036
vandong <vandong.itmpla@gmail.com>
2025-10-02 15:04:51 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/views/employees.py
+++ app/views/employees.py
@@ -1,7 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
 from . import bp
-from werkzeug.utils import secure_filename
-from app.utils.cleaning import clean_attendance_data
 import datetime
 import pandas as pd
 from flask import send_file
