5e1ac762db1bcb3e9bb007fa526631046e93cfaf
vandong <vandong.itmpla@gmail.com>
2025-10-02 14:58:58 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/views/payroll.py
+++ app/views/payroll.py
@@ -2,10 +2,12 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 import os
 from werkzeug.utils import secure_filename
 from app.utils.cleaning import clean_attendance_data
+
 import pandas as pd
 from flask import send_file
-from app.models import Employee, Payroll, db
-
+import io
+from app.models import Employee, AttendanceLog, Payroll, db
+import re
 
 # Import dữ liệu Payroll
 @bp.route("/import_payroll/<filename>", methods=["POST"])
