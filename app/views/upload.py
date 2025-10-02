7438b93afcdb279480064823a11adf502c6c4655
vandong <vandong.itmpla@gmail.com>
2025-10-02 14:49:04 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/views/upload.py
+++ app/views/upload.py
@@ -2,7 +2,16 @@ from flask import render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import os
 from . import bp
-
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+import os
+from werkzeug.utils import secure_filename
+from app.utils.cleaning import clean_attendance_data
+import datetime
+import pandas as pd
+from flask import send_file
+import io
+from app.models import Employee, AttendanceLog, Payroll, db
+import re
 
 ALLOWED_EXT = {".xls", ".xlsx"}
 
