5a97719fb2cbf7a673093e83f5778b4605af8152
vandong <vandong.itmpla@gmail.com>
2025-10-04 15:29:34 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/views/payroll.py
+++ app/views/payroll.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from app.models import  db
 from . import bp
 from app.models import  Holiday, db
-from flask import request, record
+from flask import request
 
 
 def _parse_holidays_for_month(holidays_config, ref_date):
