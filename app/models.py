17007646c0fbe1dc809bf5d23e3f7827c1232cd8
vandong <vandong.itmpla@gmail.com>
2025-10-04 15:14:39 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/models.py
+++ app/models.py
@@ -56,6 +56,7 @@ class Holiday(db.Model):
     def __repr__(self):
         return f"<Holiday {self.date} - {self.name}>"
     
+    
 class PayrollRecord(db.Model):
     __tablename__ = "payroll_records"
 
