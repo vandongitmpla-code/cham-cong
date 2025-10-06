e9258d3c05edc7b41b0fdbf3df4ef9217f036840
vandong <vandong.itmpla@gmail.com>
2025-10-06 15:17:46 +0700

    Auto backup: ${currentDate} ${currentTime}

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Employee(db.Model):
     contract_type = db.Column(db.String(50))
     salary_base = db.Column(db.Float, default=0)
     att_code = db.Column(db.String(50), unique=True)
-    start_month = db.Column(db.String(7))  # Format: YYYY-MM (ví dụ: 2025-07)
+    start_month = db.Column(db.String(7)) 
     insurance_start_month = db.Column(db.String(7))  
 
     # Quan hệ
